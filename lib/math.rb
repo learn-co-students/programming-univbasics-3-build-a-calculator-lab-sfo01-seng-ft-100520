abbbb8d7765fbf7fb388c455ee6c4013f74830b8
Inee Ader <xoliio@gmail.com>
2020-08-31 21:45:05 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum = (num1 + num2)
+  p sum
 end
 
 def subtraction(num1, num2)
-
+  difference = (num1 - num2)
+  p difference
 end
 
 def division(num1, num2)
-
+  quotient = (num1 / num2)
+  p quotient
 end
 
 def multiplication(num1, num2)
-
+  product = (num1 * num2)
+  p product
 end
 
 def modulo(num1, num2)
-
+  modulo = (num1 % num2)
+  p modulo
 end
 
 def square_root(num)
-
+  square = Math.sqrt(num)
+  p square
 end
